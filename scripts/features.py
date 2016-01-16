88943b59649d1d65ca87e90a8948e902aca727f8
U-ROBOT\Abhishek <abhicoders@gmail.com>
2016-01-17 02:06:19 +0530

    Trigrams and ensemble for regression

--- scripts/features.py
+++ scripts/features.py
@@ -26,15 +26,16 @@ class FeatureTransformer(BaseEstimator):
 		return self
 
 	def fit_transform(self, X, y=None):
+		summary = X.summary
 		X['summary'] = X.summary.map(self.remove_periods)
 		
  		summary_and_text = X.apply(self.combine_title_summary, axis=1)
 		
-		self.tfidf_vect = TfidfVectorizer(min_df=1, ngram_range=(1, 2), stop_words = None)
+		self.tfidf_vect = TfidfVectorizer(min_df=2, ngram_range=(1, 3), stop_words = None)
 		
 		summary_and_text_without_stopwords = map(self.remove_stopwords, summary_and_text)
 		
-		numerical_features = self.numerical_features(X, summary_and_text)
+		numerical_features = self.numerical_features(X, summary_and_text, summary)
 		tfidf_features = self.tfidf_vect.fit_transform(summary_and_text)
 		
 
@@ -42,18 +43,18 @@ class FeatureTransformer(BaseEstimator):
 		truncated_svd_features = self.truncated_svd.fit_transform(tfidf_features)
 		
 		features = []
-
 		features.append(numerical_features)
 		features.append(truncated_svd_features)
 		features = np.hstack(features)
 		
 		return features	
 
-	def numerical_features(self, X, summary_and_text_without_stopwords):
+	def numerical_features(self, X, summary_and_text_without_stopwords, summary):
 		num_authors = X.authors.map(lambda x: len(x.split(';')))
 		num_tokens_in_text = [len(doc.split(' ')) for doc in summary_and_text_without_stopwords]
+		num_sentences = [ len(x.split(';')) for x in summary]
 		
-		return np.array([num_authors, num_tokens_in_text]).T
+		return np.array([num_authors, num_tokens_in_text, num_sentences]).T
 
 	def combine_title_summary(self, X):
 		summary = re.sub("[.]", " ", X['summary'])
@@ -71,14 +72,14 @@ class FeatureTransformer(BaseEstimator):
 		return re.sub("[.]", " ", summary_text)
 
 	def transform(self, X):
-		
+		summary = X.summary
 		X['summary'] = X.summary.map(self.remove_periods)
 		
  		summary_and_text = X.apply(self.combine_title_summary, axis=1)
 		
 		summary_and_text_without_stopwords = map(self.remove_stopwords, summary_and_text)
 		
-		numerical_features = self.numerical_features(X, summary_and_text)
+		numerical_features = self.numerical_features(X, summary_and_text, summary)
 		tfidf_features = self.tfidf_vect.transform(summary_and_text)
 		
 		truncated_svd_features = self.truncated_svd.transform(tfidf_features)
